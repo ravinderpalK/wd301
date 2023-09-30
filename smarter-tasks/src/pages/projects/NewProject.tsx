5d9d14b7fc242700203f1aa3eb3266371680d68f
ravinderpal <ravinderpalsinghkhokhar@gmail.com>
2023-10-01 00:05:39 +0530

    level 8 commit

--- smarter-tasks/src/pages/projects/NewProject.tsx
+++ smarter-tasks/src/pages/projects/NewProject.tsx
@@ -93,6 +93,7 @@ const NewProject = () => {
                       <input
                         type="text"
                         placeholder='Enter project name...'
+                        id='name'
                         autoFocus
                         {...register('name', { required: true })}
                         className={`w-full border rounded-md py-2 px-3 my-4 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 focus:shadow-outline-blue ${errors.name ? 'border-red-500' : ''
@@ -102,7 +103,7 @@ const NewProject = () => {
                       <button type="submit" className="inline-flex justify-center rounded-md border border-transparent bg-blue-600 px-4 py-2 mr-2 text-sm font-medium text-white hover:bg-blue-500 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2">
                         Submit
                       </button>
-                      <button type="submit" onClick={closeModal} className="inline-flex  justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2">
+                      <button type="submit" id='submitNewProjectBtn' onClick={closeModal} className="inline-flex  justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2">
                         Cancel
                       </button>
                     </form>
