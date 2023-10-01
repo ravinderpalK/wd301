90e357b1d6c6a22ebc87ed13ada393e3fb90af1f
ravinderpal <ravinderpalsinghkhokhar@gmail.com>
2023-10-01 16:59:39 +0530

    level 9 commit

--- smarter-tasks/src/routes/index.tsx
+++ smarter-tasks/src/routes/index.tsx
@@ -8,7 +8,7 @@ import Members from "../pages/members";
 import Logout from "../pages/logout";
 import { MembersProvider } from "../context/members/context";
 import { NotFound } from "../pages/Notfound";
-import ProtectedRoutes from "./ProtectedRoutes";
+import ProtectedRoutes from "./ProtectedRoute";
 import ProjectContainer from "../pages/projects/ProjectContainer";
 import ProjectDetails from "../pages/project_details";
 import NewTask from "../pages/tasks/NewTask";
