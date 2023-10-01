46947db676d09ffea8268481ff5f337f55ba1e85
ravinderpal <ravinderpalsinghkhokhar@gmail.com>
2023-10-01 22:31:55 +0530

    level 9 commit updated build outDir

--- smarter-tasks/dev-dist/sw.js
+++ smarter-tasks/dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-16108a29'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.bntm309q5v"
+    "revision": "0.sqppkheubb8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
