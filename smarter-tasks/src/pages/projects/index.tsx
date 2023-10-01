1b3d2106fa5f0e2489b6999734465cb8ad3c39a0
ravinderpal <ravinderpalsinghkhokhar@gmail.com>
2023-10-01 17:17:41 +0530

    level 9 commit updated

--- smarter-tasks/src/pages/projects/index.tsx
+++ smarter-tasks/src/pages/projects/index.tsx
@@ -1,14 +1,18 @@
 
 import React, { Suspense } from "react";
 const ProjectList = React.lazy(() => import("./ProjectList"));
-import NewProject from "./NewProject";
+const NewProject = React.lazy(() => import("./NewProject"));
 import ErrorBoundary from "../../components/ErrorBoundary";
 const Projects = () => {
   return (
     <>
       <div className="flex justify-between">
         <h2 className="text-2xl font-medium tracking-tight">Projects</h2>
-        <NewProject />
+        <ErrorBoundary>
+          <Suspense fallback={<div className="suspense-loading">Loading...</div>}>
+            <NewProject />
+          </Suspense>
+        </ErrorBoundary>
       </div>
       <ErrorBoundary>
         <Suspense fallback={<div className="suspense-loading">Loading...</div>}>
