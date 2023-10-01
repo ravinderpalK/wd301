4600f54e7dc9ebd22867b25dfa407451badd694c
ravinderpal <ravinderpalsinghkhokhar@gmail.com>
2023-10-01 18:22:09 +0530

    level 9 commit updated

--- smarter-tasks/dev-dist/sw.js
+++ smarter-tasks/dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-16108a29'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.dvrp7mi0fmo"
+    "revision": "0.ecp0qm574bo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
